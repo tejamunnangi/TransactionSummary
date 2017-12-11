e3242a127cd9ea248dd35f812ef3392c953cf359
tejamunnangi <tejamunnangi@gmail.com>
2017-12-12 00:12:53 +1100

    Moving interfaces to the correct package.

---
+++ TransactionSummary/src/com/service/interfaces/ProductService.java
@@ -0,0 +1,10 @@
+package com.service.interfaces;
+
+import com.beans.Product;
+
+public interface ProductService {
+	/*
+	 * Combines and/or manipulates the content of product object for output consumption.
+	 */
+	public Product getProductInfo();
+}
---
+++ TransactionSummary/src/com/service/interfaces/TransactionAmountService.java
@@ -0,0 +1,10 @@
+package com.service.interfaces;
+
+import com.beans.TransactionAmount;
+
+public interface TransactionAmountService {
+	/*
+	 * Returns the calculated total transaction amount per client per product.
+	 */
+	public TransactionAmount getTransactionSummary();
+}
